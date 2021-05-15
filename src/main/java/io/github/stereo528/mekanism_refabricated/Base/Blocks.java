0c8960d37e1e8f00486ee16dde3381bef697fc8a
Stereo528 <52116771+Stereo528@users.noreply.github.com>
2021-05-14 23:07:04 -0600

    Ores + Salt

--- src/main/java/io/github/stereo528/mekanism_refabricated/Base/Blocks.java
+++ src/main/java/io/github/stereo528/mekanism_refabricated/Base/Blocks.java
@@ -6,26 +6,54 @@ import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import static io.github.stereo528.mekanism_refabricated.Mekanism_refabricated.MEKANISM_BLOCKS;
 import static io.github.stereo528.mekanism_refabricated.Mekanism_refabricated.MOD_ID;
-import static net.minecraft.block.Blocks.IRON_BLOCK;
+import static net.minecraft.block.Blocks.*;
 
 public class Blocks {
+    public static Logger LOGGER = LogManager.getLogger();
 
     //Metal Blocks
-    public static final Block BLOCK_BRONZE = register("block_bronze", Metal());
-    public static final Block BLOCK_COPPER = register("block_copper", Metal());
-    public static final Block BLOCK_LEAD = register("block_lead", Metal());
-    public static final Block BLOCK_OSMIUM = register("block_osmium", Metal());
-    public static final Block BLOCK_STEEL = register("block_steel", Metal());
-    public static final Block BLOCK_TIN = register("block_tin", Metal());
-    public static final Block BLOCK_URANIUM = register("block_uranium", Metal());
+    public static final Block BLOCK_BRONZE = register("block_bronze", Metal("iron"));
+    public static final Block BLOCK_COPPER = register("block_copper", Metal("iron"));
+    public static final Block BLOCK_LEAD = register("block_lead", Metal("iron"));
+    public static final Block BLOCK_OSMIUM = register("block_osmium", Metal("iron"));
+    public static final Block BLOCK_STEEL = register("block_steel", Metal("diamond"));
+    public static final Block BLOCK_TIN = register("block_tin", Metal("iron"));
+    public static final Block BLOCK_URANIUM = register("block_uranium", Metal("diamond"));
+    //Ores
+    public static final Block ORE_COPPER = register("copper_ore", Ore("iron"));
+    public static final Block ORE_LEAD = register("lead_ore", Ore("iron"));
+    public static final Block ORE_OSMIUM = register("osmium_ore", Ore("iron"));
+    public static final Block ORE_TIN = register("tin_ore", Ore("iron"));
+    public static final Block ORE_URANIUM = register("uranium_ore", Ore("diamond"));
+    //Materials
+    public static final Block SALT_BLOCK = register("block_salt", new Block(FabricBlockSettings.copy(CLAY)));
 
 
 
-    private static Block Metal() {
-        return new Block(FabricBlockSettings.copy(IRON_BLOCK));
+
+    private static Block Metal(String type) {
+        if(type == "iron") {
+            return new Block(FabricBlockSettings.copy(IRON_BLOCK));
+        }
+        if(type == "diamond") {
+            return new Block(FabricBlockSettings.copy(DIAMOND_BLOCK));
+        }
+        return null;
+    }
+    private static Block Ore(String type) {
+        if(type == "iron") {
+            return new Block(FabricBlockSettings.copy(IRON_ORE));
+        }
+        if(type == "diamond") {
+            return new Block(FabricBlockSettings.copy(DIAMOND_ORE));
+        }
+        return null;
     }
 
     private static Block register(String name, Block block) {
@@ -35,5 +63,8 @@ public class Blocks {
 
     public static void init() {
 
+    }
+    public static void log(Level level, String message){
+        LOGGER.log(level, "["+MOD_ID+"] " + message);
     }
 }
